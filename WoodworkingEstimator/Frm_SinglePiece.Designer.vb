7e1a0e7564f2160d159357b197f69ef4bbdfd0b1
wlp3333 <44732660+wlp3333@users.noreply.github.com>
2020-10-13 01:03:24 -0400

    Still not done
    Working on getting the tet boxes positioned correctly snd named just right.

--- WoodworkingEstimator/Frm_SinglePiece.Designer.vb
+++ WoodworkingEstimator/Frm_SinglePiece.Designer.vb
@@ -23,63 +23,321 @@ Partial Class Frm_SinglePiece
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.btn_solve = New System.Windows.Forms.Button()
         Me.btn_clear = New System.Windows.Forms.Button()
         Me.tb_console = New System.Windows.Forms.TextBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.TextBox13 = New System.Windows.Forms.TextBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.TextBox14 = New System.Windows.Forms.TextBox()
+        Me.TextBox15 = New System.Windows.Forms.TextBox()
+        Me.GroupBox5 = New System.Windows.Forms.GroupBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.GroupBox5.SuspendLayout()
         Me.SuspendLayout()
         '
         'GroupBox1
         '
+        Me.GroupBox1.Controls.Add(Me.GroupBox4)
+        Me.GroupBox1.Controls.Add(Me.GroupBox3)
         Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(360, 338)
+        Me.GroupBox1.Size = New System.Drawing.Size(160, 250)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Input"
         '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(58, 68)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(50, 20)
+        Me.TextBox3.TabIndex = 13
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(58, 42)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(50, 20)
+        Me.TextBox2.TabIndex = 12
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(58, 16)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(50, 20)
+        Me.TextBox1.TabIndex = 11
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 71)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(46, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Quantity"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(17, 45)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Width"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Length"
+        '
         'GroupBox2
         '
-        Me.GroupBox2.Location = New System.Drawing.Point(378, 12)
+        Me.GroupBox2.Controls.Add(Me.GroupBox5)
+        Me.GroupBox2.Location = New System.Drawing.Point(178, 12)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(360, 338)
+        Me.GroupBox2.Size = New System.Drawing.Size(160, 250)
         Me.GroupBox2.TabIndex = 1
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Output"
         '
         'btn_solve
         '
-        Me.btn_solve.Location = New System.Drawing.Point(663, 356)
+        Me.btn_solve.Location = New System.Drawing.Point(12, 268)
         Me.btn_solve.Name = "btn_solve"
-        Me.btn_solve.Size = New System.Drawing.Size(75, 50)
+        Me.btn_solve.Size = New System.Drawing.Size(160, 50)
         Me.btn_solve.TabIndex = 2
         Me.btn_solve.Text = "Solve!"
         Me.btn_solve.UseVisualStyleBackColor = True
         '
         'btn_clear
         '
-        Me.btn_clear.Location = New System.Drawing.Point(663, 412)
+        Me.btn_clear.Location = New System.Drawing.Point(178, 268)
         Me.btn_clear.Name = "btn_clear"
-        Me.btn_clear.Size = New System.Drawing.Size(75, 26)
+        Me.btn_clear.Size = New System.Drawing.Size(100, 26)
         Me.btn_clear.TabIndex = 3
-        Me.btn_clear.Text = "Clear"
+        Me.btn_clear.Text = "Clear Console"
         Me.btn_clear.UseVisualStyleBackColor = True
         '
         'tb_console
         '
-        Me.tb_console.Location = New System.Drawing.Point(12, 356)
+        Me.tb_console.Location = New System.Drawing.Point(344, 12)
         Me.tb_console.Multiline = True
         Me.tb_console.Name = "tb_console"
         Me.tb_console.ScrollBars = System.Windows.Forms.ScrollBars.Both
-        Me.tb_console.Size = New System.Drawing.Size(645, 82)
+        Me.tb_console.Size = New System.Drawing.Size(388, 282)
         Me.tb_console.TabIndex = 4
         Me.tb_console.WordWrap = False
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.TextBox3)
+        Me.GroupBox3.Controls.Add(Me.Label3)
+        Me.GroupBox3.Controls.Add(Me.Label1)
+        Me.GroupBox3.Controls.Add(Me.Label2)
+        Me.GroupBox3.Controls.Add(Me.TextBox2)
+        Me.GroupBox3.Controls.Add(Me.TextBox1)
+        Me.GroupBox3.Location = New System.Drawing.Point(6, 19)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(128, 100)
+        Me.GroupBox3.TabIndex = 18
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Board Information"
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.TextBox13)
+        Me.GroupBox4.Controls.Add(Me.Label13)
+        Me.GroupBox4.Controls.Add(Me.Label14)
+        Me.GroupBox4.Controls.Add(Me.Label15)
+        Me.GroupBox4.Controls.Add(Me.TextBox14)
+        Me.GroupBox4.Controls.Add(Me.TextBox15)
+        Me.GroupBox4.Location = New System.Drawing.Point(6, 125)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(128, 100)
+        Me.GroupBox4.TabIndex = 19
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Piece Information"
+        '
+        'TextBox13
+        '
+        Me.TextBox13.Location = New System.Drawing.Point(58, 68)
+        Me.TextBox13.Name = "TextBox13"
+        Me.TextBox13.Size = New System.Drawing.Size(50, 20)
+        Me.TextBox13.TabIndex = 13
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(6, 71)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(46, 13)
+        Me.Label13.TabIndex = 7
+        Me.Label13.Text = "Quantity"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(12, 19)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(40, 13)
+        Me.Label14.TabIndex = 5
+        Me.Label14.Text = "Length"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(17, 45)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(35, 13)
+        Me.Label15.TabIndex = 6
+        Me.Label15.Text = "Width"
+        '
+        'TextBox14
+        '
+        Me.TextBox14.Location = New System.Drawing.Point(58, 42)
+        Me.TextBox14.Name = "TextBox14"
+        Me.TextBox14.Size = New System.Drawing.Size(50, 20)
+        Me.TextBox14.TabIndex = 12
+        '
+        'TextBox15
+        '
+        Me.TextBox15.Location = New System.Drawing.Point(58, 16)
+        Me.TextBox15.Name = "TextBox15"
+        Me.TextBox15.Size = New System.Drawing.Size(50, 20)
+        Me.TextBox15.TabIndex = 11
+        '
+        'GroupBox5
+        '
+        Me.GroupBox5.Controls.Add(Me.TextBox8)
+        Me.GroupBox5.Controls.Add(Me.Label7)
+        Me.GroupBox5.Controls.Add(Me.TextBox4)
+        Me.GroupBox5.Controls.Add(Me.Label4)
+        Me.GroupBox5.Controls.Add(Me.Label5)
+        Me.GroupBox5.Controls.Add(Me.Label6)
+        Me.GroupBox5.Controls.Add(Me.TextBox5)
+        Me.GroupBox5.Controls.Add(Me.TextBox6)
+        Me.GroupBox5.Location = New System.Drawing.Point(6, 19)
+        Me.GroupBox5.Name = "GroupBox5"
+        Me.GroupBox5.Size = New System.Drawing.Size(128, 206)
+        Me.GroupBox5.TabIndex = 20
+        Me.GroupBox5.TabStop = False
+        Me.GroupBox5.Text = "Estimated Information"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(51, 68)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(50, 20)
+        Me.TextBox4.TabIndex = 13
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(5, 71)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Pieces"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(6, 19)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(33, 13)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Have"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(6, 45)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(33, 13)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "Need"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(51, 42)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(50, 20)
+        Me.TextBox5.TabIndex = 12
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(51, 16)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(50, 20)
+        Me.TextBox6.TabIndex = 11
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(178, 300)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(554, 20)
+        Me.TextBox7.TabIndex = 14
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(284, 268)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(54, 26)
+        Me.Button1.TabIndex = 15
+        Me.Button1.Text = "Export"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(50, 94)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(50, 20)
+        Me.TextBox8.TabIndex = 15
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(5, 97)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(38, 13)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Usage"
         '
         'Frm_SinglePiece
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(744, 442)
+        Me.ClientSize = New System.Drawing.Size(744, 322)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox7)
         Me.Controls.Add(Me.tb_console)
         Me.Controls.Add(Me.btn_clear)
         Me.Controls.Add(Me.btn_solve)
@@ -87,6 +345,14 @@ Partial Class Frm_SinglePiece
         Me.Controls.Add(Me.GroupBox1)
         Me.Name = "Frm_SinglePiece"
         Me.Text = "WoodworkingEstimator | Single Piece"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
+        Me.GroupBox5.ResumeLayout(False)
+        Me.GroupBox5.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -97,4 +363,29 @@ Partial Class Frm_SinglePiece
     Friend WithEvents btn_solve As Button
     Friend WithEvents btn_clear As Button
     Friend WithEvents tb_console As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents TextBox13 As TextBox
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents TextBox14 As TextBox
+    Friend WithEvents TextBox15 As TextBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents GroupBox5 As GroupBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Label7 As Label
 End Class
